185a4b1724fef34598644205052104a58ac428c6
pepe lopez <plopez741@gmail.com>
2022-02-23 10:14:22 -0800

    commit tree 9513b1b93bb6485af744edc8d9ba8ef2dbdf19c2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    const num =(42-someValue);
+    return Math.abs(num);
+}
+function distanceFromHqInFeet(someValue){
+ const blocks = distanceFromHqInBlocks(someValue);
+ return (blocks * 264)
+}
+function distanceTravelledInFeet(start, end){
+    const blocksTravelled = Math.abs(start-end);
+    return (blocksTravelled * 264)
+
+}
+function calculatesFarePrice(start, end){
+    const feet = distanceTravelledInFeet(start,end);
+    if(feet <= 400 ){
+        return 0
+    }else if(feet <= 2000){
+        return ((feet-400)*.02)
+    }else if(feet <2500){
+        return (25)
+    }else {
+        return 'cannot travel that far';
+    }
+}
